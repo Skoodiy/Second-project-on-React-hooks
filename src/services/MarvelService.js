055364938b8afa73adffbad30e6cc2979c63591b
Kanstantsin Muski <kostikla1010044@gmail.com>
2023-02-19 22:13:28 +0100

    Create logic in charInfo, charList and randomChar. Create errorMessage and spinner. Edit services. 153

---
+++ src/services/MarvelService.js
@@ -0,0 +1,39 @@
+class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=72b1594178d077c8f8fe674cc8a53c00';
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+  getAllCharacters = async () => {
+      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+      return res.data.results.map(this._transformCharacter)
+  }
+
+  getCharacter = async (id) => {
+      const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+      return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+
+    return {
+      name: char.name,
+      description: (char.description) ? char.description : 'Description is missing',
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      id: char.id,
+      comics: char.comics.items
+    }
+  }
+}
+
+export default MarvelService;
